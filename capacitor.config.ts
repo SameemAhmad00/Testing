fbb0a56384c5a5a4529f2bb7327c09a024cd4919
SameemAhmad00 <118016523+SameemAhmad00@users.noreply.github.com>
2026-08-13 02:18:34 +0530

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.sameem.chat',
-  appName: 'Sameem Chat',
-  webDir: 'dist'
-};
-
-export default config;
